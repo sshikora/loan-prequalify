dbd0cbbf294fbdcaf08a8c13e9e62a8b2348d2a4
Seth Shikora <seth@shikora.org>
2020-02-12 00:10:03 -0800

    Updates

---
+++ precache-manifest.4d4f2eaafdd5e2e4fd7fdc1c09e4ce3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4332c156f9ba3eb3b40a95173ad4820c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6268743e6dede24cb14",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "573aa6a8b457c1226116",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a6268743e6dede24cb14",
+    "url": "/static/js/2.e1511239.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.e1511239.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "573aa6a8b457c1226116",
+    "url": "/static/js/main.d748bf26.chunk.js"
+  },
+  {
+    "revision": "fb4ad9d58d9ad968719f",
+    "url": "/static/js/runtime-main.ee04bc26.js"
+  }
+]);
